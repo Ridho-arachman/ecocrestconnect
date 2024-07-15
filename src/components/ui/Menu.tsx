4573ca94327a24589693d00f0e5c653686453823
Ridho-arachman <ridho.arachman22@gmail.com>
2024-07-15 15:29:38 +0700

    menambahkan icon.ico dan merapihkan navbar dan menu

--- src/components/ui/Menu.tsx
+++ src/components/ui/Menu.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -7,36 +8,72 @@ type Props = {
 };
 
 const Menu: React.FC<Props> = ({ href = "/", type }) => {
-  return (
-    <>
-      {type === "sm-md" && (
-        <ul className="hidden lg:flex menu menu-horizontal px-1">
-          <li>
-            <Link href={href}>Beranda</Link>
-          </li>
-          <li>
-            <Link href={"/"}>Tentang Kami</Link>
-          </li>
-          <li>
-            <details>
-              <summary>Layanan</summary>
-              <ul className="p-2">
-                <li>
-                  <Link href={"/"}>Artikel</Link>
-                </li>
-                <li>
-                  <Link href={"/"}>Acara</Link>
-                </li>
-                <li>
-                  <Link href={"/"}>Komunitas</Link>
-                </li>
-              </ul>
-            </details>
-          </li>
-        </ul>
-      )}
-    </>
+  const menuSmMd = (
+    <ul className="hidden lg:flex menu menu-horizontal px-1">
+      <li>
+        <Link href={href}>Beranda</Link>
+      </li>
+      <li>
+        <Link href={"/"}>Tentang Kami</Link>
+      </li>
+      <li>
+        <details>
+          <summary>Layanan</summary>
+          <ul className="p-2">
+            <li>
+              <Link href={"/"}>Artikel</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Acara</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Komunitas</Link>
+            </li>
+          </ul>
+        </details>
+      </li>
+    </ul>
   );
+  const menuLg = (
+    <ul className="menu lg:hidden fixed z-10 top-0 left-0 right-0 w-[100vw] h-max pt-16 bg-base-200">
+      <li className="flex flex-row items-center justify-start">
+        <Image src={"/logo.png"} alt="Gambar Logo" width={100} height={100} />
+      </li>
+      <li>
+        <a>Item 1</a>
+      </li>
+      <li>
+        <details open>
+          <summary>Parent</summary>
+          <ul>
+            <li>
+              <a>Submenu 1</a>
+            </li>
+            <li>
+              <a>Submenu 2</a>
+            </li>
+            <li>
+              <details open>
+                <summary>Parent</summary>
+                <ul>
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <a>Item 3</a>
+      </li>
+    </ul>
+  );
+  return <>{type === "sm-md" ? menuSmMd : menuLg}</>;
 };
 
 export default Menu;
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
-  const handleClick = () => setOpen(!open);
+  const handleClick = (): void => setOpen(!open);
   return (
     <nav className="navbar w-full fixed top-0 shadow ">
       <div className="navbar-start flex flex-row">
@@ -19,54 +19,10 @@ const Navbar = () => {
         >
           {open ? <X /> : <MenuIcon />}
         </Button>
-
-        {open && (
-          <ul className="menu lg:hidden fixed z-10 top-0 left-0 right-0 w-[100vw] h-max pt-16 bg-base-200">
-            <li className="flex flex-row items-center justify-start">
-              <Image
-                src={"/logo.png"}
-                alt="Gambar Logo"
-                width={100}
-                height={100}
-              />
-            </li>
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details open>
-                <summary>Parent</summary>
-                <ul>
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                  <li>
-                    <details open>
-                      <summary>Parent</summary>
-                      <ul>
-                        <li>
-                          <a>Submenu 1</a>
-                        </li>
-                        <li>
-                          <a>Submenu 2</a>
-                        </li>
-                      </ul>
-                    </details>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        )}
+        {open && <Menu type="lg" />}
       </div>
       <div className="navbar-center ">
-        <Image src={"/logo.png"} alt="Gambar 1" width={50} height={50} />
+        <Image src={"/logo.png"} alt="Gambar Logo" width={50} height={50} />
       </div>
       <div className="navbar-end">
         <Button className="btn btn-primary btn-md">Masuk</Button>
